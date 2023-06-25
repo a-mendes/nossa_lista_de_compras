26418e45af28517476be4e8fddd4351e59102d9e
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2023-06-25 13:38:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nossa_lista_de_compras
+ Aplicação mobile desenvolvida em Dart para a disciplina BCC323 - Engenharia de Software II
---
+++ android/app/src/main/kotlin/com/ufop/nossa_lista_de_compras/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ufop.nossa_lista_de_compras
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
